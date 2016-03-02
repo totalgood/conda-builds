fd0c940128f45d9ad7e23fd88a2f32b4d3e9e10b
Hobson Lane <hobsonlane@gmail.com>
2016-03-02 23:07:43 +1000

    twython build now works after cleaning up all patch.exe and .bat files in Scripts and /usr/bin and conda install patch and add a channel on anaconda.org by clinicalgraphics

---
+++ build_packages_win.sh
@@ -0,0 +1,226 @@
+ mkdir src
+ ls -al
+ git clone git@github.com:totalgood/python-rtmbot
+ conda install nolearn
+ mkdir src
+ ls
+ cd src
+ ssh-key-gen
+ ssh-keygen
+ more ~/.ssh
+ ls ~/.ssh
+ cat ~/.ssh/id_rsa
+ cat ~/.ssh/id_rsa.pub 
+ ssh-keygen
+ git clone git@github.com:totalgood/conda-builds
+ cd conda-builds/
+ conda install conda-build
+ conda install unxutils
+ conda install unxutils
+ conda install ipython[all]
+ conda install ipython
+ ls -al
+ conda build gdbn
+  conda config --set anaconda_upload yes
+ conda build Theano
+ conda install anaconda-client
+ anaconda login
+ conda install --upgrade anaconda-client
+ conda upgrade anaconda-client
+ conda install --channel hobs
+ anaconda login
+ which python
+ exit
+ anaconda login
+ conda install --channel hobs theano
+ anaconda search -t conda theano
+ winpty
+ winpty ipython
+ winpty anaconda login
+ conda build nolearn
+ cd conda-builds
+ cd src
+ cd conda-builds/
+ conda build nolearn
+ ls
+ conda build gnumpy
+ conda build gdbn/
+ conda skeleton slackclient
+ conda skeleton pypislackclient
+ conda skeleton pypi \slackclient
+ conda skeleton pypi slackclient
+ fg
+ ps aux
+ exit
+ ps aux
+ conda skeleton pypi slackclient
+ ls C:/
+ ls C:/Anaconda2/conda-bld/
+ rm C:/Anaconda2/conda-bld/.conda_lock-5656
+ ls C:/Anaconda2/conda-bld/
+ ls C:/Anaconda2/conda-bld/.conda_lock-5656
+ conda skeleton pypi slackclient
+ ls -al C:/Anaconda2/conda-bld/
+ rm -rf C:/Anaconda2/conda-bld/.conda_lock-1476
+ conda skeleton pypi slackclient
+ pwd
+ exit
+ pwd
+ ls
+ cd dev
+ ls
+ cd ..
+ ls
+ cd ~/src
+ ls -al
+ pwd
+ cd conda-builds
+ conda skeleton pypi slackclient
+ cd src
+ cd conda-builds/
+ winpty conda skeleton pypi slackclient
+ winpty conda skeleton pypi nlup
+ ls -al
+ cd nlup
+ more core.py
+ lexit
+ exit
+ cd src/conda-builds/; conda skeleton pypi nlup
+ exit
+ cd ~/src
+ cd conda-builds/
+ ls -al
+ conda build slacker
+ conda build lasagne
+ ls -al
+ conda build joblib
+ ls -al
+ conda build tabulate
+ conda build gdbn
+ conda build gnumpy
+ conda build nolearn
+ conda install nolearn
+ anaconda search -t conda nolearn 
+ conda install hobs/nolearn
+ ds (master)
+ $
+ cond config --add-channels hobs
+ conda config --add-channels hobs
+ conda config --add channels hobs
+ conda install nolearn
+ conda install nolearn==0.7
+ conda install nolearn ==0.7
+ conda install theano
+ anaconda search -t conda theano
+ anaconda show hobs/theano
+ conda config --add channels https://conda.anaconda.org/hobs
+ conda install nolearn
+ conda install theano
+ anaconda show hobs/theano
+  conda install --channel https://conda.anaconda.org/hobs theano
+ anaconda search -t conda theano
+ conda build theano
+ conda install theano
+ conda build lasagne
+ conda install nolearn
+ ls -al
+ conda build theano
+ cd nolearn
+ cat meta.yml
+ cat meta.yaml
+ cd ..
+ cd ..
+ cd conda-builds/
+ git status
+ git add slackclient/
+ git push
+ git config --global push.default simple
+ git config list
+ git config --global show
+ git config show
+ git config read
+ git config -h
+ git config --git-all
+ git config --global --get-all
+ git config --get-all
+ winpty git config --get-all
+ winpty git config --get-all user
+ winpty git config --get-all all
+ winpty git config --get-all global
+ winpty git config --get-all main
+ winpty git config --get user
+ winpty git config --get user.name
+ winpty git config --get user.email
+ git config --global user.email hobsonlane@gmail.com
+ git config --global user.name "Hobson Lane"
+ cd ~
+ cd src
+ ls
+ git clone git@github.com:hackoregon/hack-university-machine-learning.git
+ git clone git@github.com:totalgood/python-rtmbot.git
+ cd python-rtmbot/
+ winpty python main.py
+ conda install slackclient
+ anaconda search -t conda slackclient
+ cd ..
+ cd conda-builds/
+ conda build slackclient
+ conda skeleton pypi websocket-client
+ conda build websocket-client
+ conda build slackclient
+ conda skeleton pypi twython
+ conda install twython
+ conda build twython
+ conda build pypi requests_oathlib
+ conda skeleton pypi requests_oathlib
+ conda skeleton pypi requests
+ conda skeleton pypi requests-oathlib
+ conda skeleton pypi requests-oauthlib
+ conda build requests-oauthlib
+ conda install patch
+ conda upgrade patch
+ which patdh
+ which patch
+ conda skeleton pypi requests-oauthlib
+ which patch.bat
+ conda uninstall patch
+ which patch
+ mv /usr/bin/patch /usr/bin/broken-path2.5.exe
+ which patch
+ rm /c/Anaconda2/Scripts/patch
+ conda install patch
+ which patch
+ conda skeleton pypi requests-oauthlib
+ conda build requests-oauthlib
+ conda skeleton pypi oauthlib
+ conda build oauthlib
+ conda skeleton pypi blinker
+ conda build blinker
+ conda build oauthlib
+ conda skeleton pypi pyjwt
+ conda build pyjwt
+ conda skeleton pypi nlup
+ conda build nlup
+ conda skeleton pypi jsonpickle
+ conda build jsonpickle
+ conda build nlup
+ conda install nltk
+ git status
+ git add .
+ git commit -am 'dependencies for slackclient and nlup, neither completed build'
+ git push
+ conda build twython
+ conda build slackclient
+ conda build oauthlib
+ conda install pyjwt
+ anaconda search -t conda pyjwt
+ 
+ conda config --add channels https://conda.anaconda.org/clinicalgraphics
+ conda install pyjwt
+ conda build oauthlib
+ conda build requests-oauthlib
+ history
+ hsitory | cut -c 5-
+ history | cut -c 5-
+ history | cut -c 7-
+ history | cut -c 7- > build_packages_win.sh
